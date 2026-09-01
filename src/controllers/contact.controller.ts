5f53894c3a0a255618a75bd4410828d8b7bbe419
Mario Brino <mariobrino@gmail.com>
2026-09-01 16:07:58 -0600

    refactor: move createSchema definition to the top and improve error handling in update and delete methods

--- src/controllers/contact.controller.ts
+++ src/controllers/contact.controller.ts
@@ -5,6 +5,16 @@ import type { ContactData } from "@/models/contact.model";
 import { createContact, deleteContact, findContactById, getAllContacts, updateContact } from "@/models/contact.model";
 
 class ContactController {
+  createSchema = z.object({
+    body: z.object({
+      firstName: z.string(),
+      lastName: z.string(),
+      email: z.email(),
+      favoriteColor: z.string(),
+      birthday: z.coerce.date(),
+    }) satisfies z.ZodType<ContactData>,
+  });
+
   index = async (_request: Request, response: Response): Promise<Response> => {
     const contacts = await getAllContacts();
 
@@ -24,16 +34,6 @@ class ContactController {
     return response.status(200).json(contact);
   };
 
-  createSchema = z.object({
-    body: z.object({
-      firstName: z.string(),
-      lastName: z.string(),
-      email: z.email(),
-      favoriteColor: z.string(),
-      birthday: z.coerce.date(),
-    }) satisfies z.ZodType<ContactData>,
-  });
-
   store = async (request: Request, response: Response): Promise<Response> => {
     const contactData: ContactData = request.body;
     const createdContact = await createContact(contactData);
@@ -42,21 +42,32 @@ class ContactController {
 
   update = async (request: Request, response: Response): Promise<Response> => {
     const { id } = request.params;
-    const updatedData: Partial<ContactData> = request.body;
+    const contact = await findContactById(id as string);
 
     response.setHeader("Content-Type", "application/json");
-    const contact = await updateContact(id as string, updatedData);
     if (!contact) {
       return response.status(404).json({ message: "Contact not found" });
     }
 
-    return response.status(200).json(contact);
+    const updatedData: Partial<ContactData> = request.body;
+
+    const contactUpdated = await updateContact(id as string, updatedData);
+    if (!contactUpdated) {
+      return response.status(404).json({ message: "Contact not found" });
+    }
+
+    return response.status(200).json(contactUpdated);
   };
 
   delete = async (request: Request, response: Response): Promise<Response> => {
     const { id } = request.params;
+    const contact = await findContactById(id as string);
 
     response.setHeader("Content-Type", "application/json");
+    if (!contact) {
+      return response.status(404).json({ message: "Contact not found" });
+    }
+
     const contactDeleted = await deleteContact(id as string);
     if (!contactDeleted) {
       return response.status(404).json({ message: "Contact not found" });
