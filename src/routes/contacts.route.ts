6b3f504c937552fcfadb9f467d687c3d5452e6e8
Mario Brino <mariobrino@gmail.com>
2026-09-01 00:32:20 -0600

    fix: change contacts POST to PUT and DELETE

--- src/routes/contacts.route.ts
+++ src/routes/contacts.route.ts
@@ -8,7 +8,7 @@ const router = Router();
 router.get("/", contactController.index);
 router.get("/:id", contactController.show);
 router.post("/", validateMiddleware(contactController.createSchema), contactController.store);
-router.post("/:id/update", contactController.update);
-router.post("/:id/delete", contactController.delete);
+router.put("/:id/update", contactController.update);
+router.delete("/:id/delete", contactController.delete);
 
 export { router as contactRoutes };
