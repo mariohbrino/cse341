e64d5c8200bc10872e7424c51689ad824ed5a09f
Mario Brino <mariobrino@gmail.com>
2026-09-09 20:35:21 -0600

    fix: add BASE_URL to .env.example and update openapi host configuration

--- src/openapi.ts
+++ src/openapi.ts
@@ -3,6 +3,8 @@ import swaggerAutogen from "swagger-autogen";
 import { version } from "../package.json" with { type: "json" };
 
 const PORT = process.env["PORT"] || 3000;
+const PRODUCTION = process.env["NODE_ENV"] === "production";
+const BASE_URL = PRODUCTION ? process.env["BASE_URL"] : `localhost:${PORT}`;
 
 const outputFile = "./openapi.json";
 const routes = ["./routes/index.route"];
@@ -12,7 +14,8 @@ const doc = {
     description: "A simple API for the CSE341 course",
     version: version,
   },
-  host: `localhost:${PORT}`,
+  host: BASE_URL,
+  schemes: PRODUCTION ? ["https"] : ["http", "https"],
 };
 
 swaggerAutogen()(outputFile, routes, doc);
