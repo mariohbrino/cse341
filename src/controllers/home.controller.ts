d6f5cf64c15972014b7b416d5a3a40787804791a
Mario Brino <mariobrino@gmail.com>
2026-09-16 22:36:23 -0600

    feat: enhance home controller with error handling and add invalid route

--- src/controllers/home.controller.ts
+++ src/controllers/home.controller.ts
@@ -1,9 +1,26 @@
 import type { Request, Response } from "express";
 
 class HomeController {
-  index = async (_request: Request, response: Response) => {
+  index = async (request: Request, response: Response) => {
+    void request;
+    try {
+      response.setHeader("Content-Type", "application/json");
+      return response.json({ message: "Hello, World!" });
+    } catch (error) {
+      console.error(error);
+      return response.status(500).json({ error: "Internal Server Error" });
+    }
+  };
+
+  invalid = async (request: Request, response: Response) => {
+    void request;
     response.setHeader("Content-Type", "application/json");
-    response.send(JSON.stringify({ message: "Hello, World!" }));
+    try {
+      throw new Error("Something went wrong");
+    } catch (error) {
+      console.error(error);
+      return response.status(500).json({ error: "Something went wrong" });
+    }
   };
 }
 
--- src/routes/home.route.ts
+++ src/routes/home.route.ts
@@ -1,15 +1,36 @@
+import type { Request, Response } from "express";
 import express from "express";
 
 import { homeController } from "@/controllers/home.controller";
 
 const router = express.Router();
 
-router.get("/", (request: express.Request, response: express.Response) => {
+router.get("/", (request: Request, response: Response) => {
   /**
    * #swagger.tags = ['Root']
    * #swagger.description = 'Get the home page'
+   * * #swagger.responses[200] = {
+      description: 'Successful home page retrieval response',
+      content: { "application/json": { schema: { type: "object", example: { message: "Hello, World!" } } } }
+    }
+   * #swagger.responses[500] = {
+      description: 'Internal server error response',
+      content: { "application/json": { schema: { type: "object", example: { error: "Internal Server Error" } } } }
+    }
    */
   return homeController.index(request, response);
+});
+
+router.get("/invalid", (request: Request, response: Response) => {
+  /**
+   * #swagger.tags = ['Root']
+   * #swagger.description = 'Get the invalid route page'
+   * #swagger.responses[500] = {
+      description: 'Internal server error response',
+      content: { "application/json": { schema: { type: "object", example: { error: "Something went wrong" } } } }
+    }
+   */
+  return homeController.invalid(request, response);
 });
 
 export { router as homeRoutes };
